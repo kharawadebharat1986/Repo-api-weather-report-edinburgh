20aabe711df72f8beb0cb1b5d1c4b19fa33d8317
Bharat Kharawade <bharat.kharawade@gmail.com>
2022-03-29 21:55:33 +0100

    api first check in

---
+++ Application/aspnet-dotnet-core-api/Controllers/WeatherForecastController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace aspnet_dotnet_core_api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public WeatherForecast Get()
+        {
+            return new WeatherForecast() { Date=DateTime.Now, Summary="Weather report", TemperatureC=30};
+        }
+    }
+}
